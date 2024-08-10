ceb71800e3282a4930b158345d7ca16773649536
feliperocha021 <112943971+feliperocha021@users.noreply.github.com>
2024-08-10 15:12:30 -0300

    Add files via upload

---
+++ mult-max3.asm
@@ -0,0 +1,92 @@
+.text
+    # Reservar espaço na pilha para a matrizA (2x2 elementos de 4 bytes cada), a matrizB (2x2 elementos de 4 bytes cada)
+    #  e a matrizC resultante (2x2 elementos de 4 bytes cada)
+    addi $sp, $sp, -64
+
+    # Armazenar os valores na matrizA 2x2 por linha
+    li $t0, 2
+    sw $t0, 0($sp)
+    li $t0, 3
+    sw $t0, 4($sp)
+    li $t0, 2
+    sw $t0, 8($sp)
+    li $t0, 2
+    sw $t0, 12($sp)
+    li $t0, 1
+    sw $t0, 16($sp)
+    li $t0, 2
+    sw $t0, 20($sp)
+    # Armazenar os valores da matrizB 2x2 na forma transposta para facilitar o cáculo fazendo primeira linha de A com primeira linha B, primeira linha de A com a segunda linha de B...
+    li $t0, 2
+    sw $t0, 24($sp)
+    li $t0, 1
+    sw $t0, 28($sp)
+    li $t0, 1
+    sw $t0, 32($sp)
+    li $t0, 3
+    sw $t0, 36($sp)
+    # Multiplicação das mastrizes A e B
+    li $t0, 0 #indice inicial da matrizA
+    la $s0, 40($sp) #endereço da primeria linha e coluna da matrizC
+    la $s5, 0($sp) #endereço da primeria linha e coluna da matrizA
+    la $t1, 0($s5) #endereço da primeria linha e coluna da matrizA
+    la $t2, 24($sp) #endereço da primeria linha e coluna da matrizB
+    li $t8, 24 #acumulador de bytes
+    # loop1 das linhas da matrizA de 4 em 4
+    loop1:
+	beq $t0, 3, end_loop1 #linhas da matrizA
+	li $t3, 0 #variavel do loop2
+	loop2:
+		beq $t3, 2, end_loop2 #linhas da matrizB
+		li $t7, 0 #acumulador
+		li $s1, 0 #variavel do loop3
+		loop3:
+			beq $s1, 2, end_loop3 #colunas da matrizB
+			lw $t4, 0($t1) #acessando valor da matrizA
+			lw $t5, 0($t2) #acessando valor da matrizB
+			mul $t6, $t4, $t5 #multiplicando os valores das matrizes
+			add $t7, $t7, $t6 #adicionando o resultado ao acumulador
+			addi $t1, $t1, 4 #acessando endereço da matrizA
+			la $s4, 0($t1) #salvando a próxima linha da matriz A caso a sua linha anterior já foi totalmente utilizada
+			addi $t2, $t2, 4 #acessando endereço da matrizB
+			addi $t8, $t8, 4 #acumulador de bytes
+			addi $s1, $s1, 1 #incrementando a varivel do loop3
+			j loop3
+    
+    end_loop3:
+    	addi $t3, $t3, 1 #incrementando a varivel do loop2
+    	sw $t7, 0($s0) #adicionando valor na matriz C
+    	addi $s0, $s0, 4 #acessando endereço da matriz C
+    	bne $t8, 40, reseta_linha_matrizA #a linha atual da matrizA ainda não foi calculada com todas as outras linhas da matrizB
+    	la $t2, 24($sp) #endereço da primeria linha e coluna da matrizB #a linha atual da matrizA já foi calculada
+    	li $t8, 24 #acumulador de bytes
+    	j loop2
+    	
+    end_loop2:
+    	la $s5, 0($s4) #próxima linha da matrizA que será calculada
+    	addi $t0, $t0, 1 #incrementando a varivel do loop1
+    	j loop1
+    	
+    reseta_linha_matrizA:
+    	la $t1, 0($s5) #endereço do primeiro elemento da linha da matrizAa que está sendo calculada
+    	j loop2
+    	
+    # Finalizar o programa
+    end_loop1:
+    	la $s0, 40($sp) #endereço da primeria linha e coluna da matrizC
+    	li $s3, 0
+    	imprimir_matrizC:
+    		beq $s3, 6, encerra
+    		li $v0, 1
+    		lw $a0, 0($s0)
+    		syscall
+    		# Imprimir um espaço em branco
+    		li $v0, 11
+    		li $a0, 32
+    		syscall
+    		addi $s3, $s3, 1
+    		addi $s0, $s0, 4
+    		j imprimir_matrizC
+    encerra:
+    	li $v0, 10
+    	syscall
